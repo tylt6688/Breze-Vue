ae8e34407e76c0f1877887020b4268f38b88112d
LeoChan <echoleochan@foxmail.com>
2022-12-05 16:17:17 +0800

    服务器监控显示错误

--- src/api/monitor/serverMonitor.js
+++ src/api/monitor/serverMonitor.js
@@ -4,7 +4,7 @@ export default {
     // 获取CPU数据
     getLoginLogCpuList() {
         return axios({
-            url: '/monitor/server/cpuinfo',
+            url: '/monitor/server/cpu_info',
             method: 'get'
         })
     },
